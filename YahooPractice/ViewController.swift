856c21e86cf2319050b7ddd0de0b3e2616bfd9ed
takagai2 <takagai@rightcode.co.jp>
2018-06-29 14:46:25 +0900

    initial commit

---
+++ YahooPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YahooPractice
+//
+//  Created by mao takagai on 2018/06/29.
+//  Copyright © 2018年 mao takagai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
